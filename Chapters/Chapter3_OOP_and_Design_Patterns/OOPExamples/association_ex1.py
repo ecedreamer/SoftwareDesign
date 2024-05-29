c049cc5ea09c4780b504d708aea892f5db274394
Dipak Niroula <dni@logpoint.com>
2024-05-29 07:48:54 +0545

    association example added

---
+++ Chapters/Chapter3_OOP_and_Design_Patterns/OOPExamples/association_ex1.py
@@ -0,0 +1,54 @@
+"""
+Association: Association is a relationship among objects where one object is associated with other type of object. It has weeker relationship since the related objects have separate existance or lifetime. It is also one of the fundamental concepts of OOP. 
+
+Example: Student object has a Laptop object. But Student object is not composed of the Laptop object.
+"""
+
+
+class Student:
+    def __init__(self, name: str, student_id, email, program, batch):
+        self.name = name
+        self.student_id = student_id
+        self.student_email = email
+        self.program = program
+        self.batch = batch
+
+    def get_student_intro(self):
+        return self.program + "-" + str(self.batch) + "; " + self.name + "_" + str(self.student_id)
+    
+class Laptop:
+    def __init__(self, brand: str, model: str, owner: Student): # type hint
+        self.brand = brand
+        self.model = model
+        self.owner = owner
+
+    def get_laptop_info(self):
+        return self.brand + "-" + self.model
+    
+    def get_owner_info(self):
+        return self.owner.get_student_intro()
+    
+
+def main():
+    student1 = Student("Prabin Babu Kattel", 1003, "babu@gmail.com", "AI", 36)
+    student2 = Student("Rohit Shah", 1006, "rohit@gmail.com", "AI", 36)
+    laptop1 = Laptop("Lenovo", "Legion 5", student2)
+    laptop2 = Laptop("Asus", "G16", student1)
+    laptop3 = Laptop("Asus", "Purano", student2)
+
+    # intro = student1.get_student_intro()
+    # print(intro)
+
+    laptop_info = laptop1.get_laptop_info()
+    print(laptop_info)
+    print(laptop1.get_owner_info())
+
+
+main()
+
+
+
+
+
+
+        
